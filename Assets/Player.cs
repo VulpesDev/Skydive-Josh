e50f7ad1d0a98c664af889b5ade1311934bcb755
Kayakaga <baijidevelop@gmail.com>
2022-08-06 21:19:41 +0300

    Random name

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     [SerializeField] float speed;
+    [SerializeField] float magnitude;
+    bool shaking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +21,31 @@ public class Player : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector3(horizontal * speed, vertical * speed, 0f);
+        rb.velocity = new Vector3(horizontal * speed * Time.deltaTime,
+            vertical * speed * Time.deltaTime, 0f);
 
-        if (vertical > 0.01)
+        anim.SetBool("Forward", vertical > 0.01);
+        anim.SetBool("Left", horizontal < -0.01);
+        anim.SetBool("Right", horizontal > 0.01);
+        anim.SetBool("Back", vertical < -0.01);
+        if (!shaking) StartCoroutine(Shaking(magnitude));
+    }
+
+    private IEnumerator Shaking(float magnitude = 0.25f)
+    {
+        shaking = true;
+        float t = 0f, x, y;
+        while (t < 0.35f)
         {
-            anim.SetBool("Forward", true);
+            x = Random.Range(-1f, 1f) * magnitude;
+            y = Random.Range(-1f, 1f) * magnitude;
+
+            rb.transform.localPosition += new Vector3(x, y, rb.transform.position.z);
+
+            t += Time.deltaTime;
+            yield return null;
         }
-        else
-        {
-            anim.SetBool("Forward", false);
-        }
+
+        shaking = false;
     }
 }
